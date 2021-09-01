5cbf4184d00586b8d001745720e5aaad2ccdbce7
Ashutosh Kumar <57178478+Ashu111144@users.noreply.github.com>
2021-09-01 19:48:55 +0530

    Add files via upload

---
+++ GameOfLife/main.py
@@ -0,0 +1,44 @@
+import pygame
+import time
+import random
+import numpy as np
+import os
+import grid
+
+os.environ["SDL_VIDEO_CENTERED"] = '1'
+
+width, height = 1080, 720
+size = (width, height)
+
+# pygame.init()
+clock = pygame.time.Clock()
+pygame.display.set_caption("CONWAY'S GAME OF LIFE")
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+fps = 60
+
+black = (0, 0, 0)
+blue = (0, 121, 150)
+blue1 = (0, 14, 71)
+white = (255, 255, 255)
+
+scaler = 40
+offset = 1
+
+Grid = grid.Grid(width, height, scaler, offset)
+Grid.random2d_array()
+
+
+run = True
+while run:
+    clock.tick(fps)
+    screen.fill(black)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    clock.tick(60)
+    Grid.Conway(off_color=white, on_color=blue1, surface=screen)
+    pygame.display.update()
+
+pygame.quit()
